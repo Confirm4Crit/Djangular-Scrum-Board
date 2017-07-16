8d55409808f73b1c9425a22ed403699649641abc
Miles Robson <miles_robson@yahoo.com>
2017-07-16 15:48:17 -0400

    Added fields = '__all__' to serializers

--- scrumboard/serializers.py
+++ scrumboard/serializers.py
@@ -6,8 +6,10 @@ from .models import List, Card
 class ListSerializer(serializers.ModelSerializer):
     class Meta:
         model = List
+        fields = '__all__'
 
 
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
+        fields = '__all__'
